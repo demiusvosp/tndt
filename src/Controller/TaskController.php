9608d5b93de66d0692f67c93124d1271249288f2
Dmitriy Vospennikov <demius.vosp@gmail.com>
2019-11-05 17:39:39 +0300

    Тестируем лог

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -6,16 +6,20 @@
  */
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
 
 class TaskController extends AbstractController
 {
-    public function index(Request $request)
+    public function index(Request $request, LoggerInterface $log)
     {
         $input = $request->query->get('hello', 'world');
 
+        $log->info('get for: '.$input);
+        $log->notice('Notice example.', ['a' => 'b']);
+        $log->critical('Critical examle');
         return $this->render('task/index.html.twig', ['hello' => $input]);
     }
 }
