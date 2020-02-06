dbcf94a16bb72c127b48e0b6828ab22d89cada78
Dmitriy Vospennikov <demius.vosp@gmail.com>
2020-02-06 23:38:44 +0300

    Сделали нашу утилитку красивше

--- bin/db_schema.sh
+++ bin/db_schema.sh
@@ -1,13 +1,23 @@
 #! /bin/bash
 
+export $(egrep -v '^#' .env | xargs)
+echo "environment: ${APP_ENV}"
+
 if [[ $1 == 'validate' ]]; then 
-  docker exec -it yatt_php_1 ./bin/console doctrine:schema:validate
+    docker exec -it yatt_php_1 ./bin/console doctrine:schema:validate
   
 elif [[ $1 == 'update' ]]; then
-  docker exec -it yatt_php_1 bin/console doctrine:schema:update --force
+    if [[ ${APP_ENV} == 'dev' ]]; then
+        docker exec -it yatt_php_1 bin/console doctrine:schema:update --force
+
+    else
+        echo -e "\033[1;31m Run db_update safely in development. For other environments use migrations\033[0m"
+        exit -1
+
+    fi
   
 else
-  echo "Usage: ./db_schema.sh validate|update"
+    echo "Usage: ./db_schema.sh validate|update"
   
 fi
 
