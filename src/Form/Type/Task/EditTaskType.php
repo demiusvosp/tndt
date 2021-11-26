5671c726fc51470367b4e930a97bd7158757b402
Dmitriy Vospennikov <demius.vosp@gmail.com>
2021-11-27 01:47:57 +0300

    tndt-13 Выбор сложности при создании и редактировании задачи

--- src/Form/Type/Task/EditTaskType.php
+++ src/Form/Type/Task/EditTaskType.php
@@ -9,51 +9,10 @@ declare(strict_types=1);
 namespace App\Form\Type\Task;
 
 use App\Form\DTO\Task\EditTaskDTO;
-use App\Form\Type\Base\DictionarySelectType;
-use App\Form\Type\User\UserSelectType;
-use App\Service\DictionariesTypeEnum;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EditTaskType extends AbstractType
+class EditTaskType extends NewTaskType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add(
-                'caption',
-                TextType::class,
-                ['label' => 'task.caption.label', 'help' => 'task.caption.help']
-            )
-            ->add(
-                'assignedTo',
-                UserSelectType::class,
-                ['label' => 'task.assignedTo.label', 'help' => 'task.assignedTo.help']
-            )
-            ->add(
-                'type',
-                DictionarySelectType::class,
-                [
-                    'label' => 'task.type.label',
-                    'help' => 'task.type.help',
-                    'dictionary' => DictionariesTypeEnum::TASK_TYPE()
-                ]
-            )
-            ->add(
-                'description',
-                TextareaType::class,
-                [
-                    'label' => 'task.description.label',
-                    'help' => 'task.description.help',
-                    'attr' => ['rows' => 20],
-                    'required' => false,
-                    'empty_data' => '',
-                ]
-            );
-    }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
