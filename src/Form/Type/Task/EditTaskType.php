205cc977ea76779978eb50e618149647cd8ba5bb
Dmitriy Vospennikov <demius.vosp@gmail.com>
2021-12-19 18:43:34 +0300

    tndt-5 - Справочник этап задачи. MR-fixes. Перевернули наследование между формами новой задачи и редактируемой задачи.

--- src/Form/Type/Task/EditTaskType.php
+++ src/Form/Type/Task/EditTaskType.php
@@ -8,31 +8,13 @@ declare(strict_types=1);
 
 namespace App\Form\Type\Task;
 
-use App\Dictionary\Fetcher;
-use App\Dictionary\TypesEnum;
-use App\Entity\Contract\HasClosedStatusInterface;
-use App\Entity\Task;
 use App\Form\DTO\Task\EditTaskDTO;
-use App\Form\Type\Base\DictionarySelectType;
 use App\Form\Type\Base\DictionaryStageSelectType;
-use App\Form\Type\User\UserSelectType;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EditTaskType extends AbstractType
+class EditTaskType extends NewTaskType
 {
-    private Fetcher $dictionaryFetcher;
-
-    public function __construct(Fetcher $dictionaryFetcher)
-    {
-        $this->dictionaryFetcher = $dictionaryFetcher;
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $this->buildCommonFields($builder);
@@ -44,106 +26,4 @@ class EditTaskType extends AbstractType
         $resolver->setDefault('data_class', EditTaskDTO::class);
     }
 
-    protected function buildCommonFields(FormBuilderInterface $builder): void
-    {
-        $builder
-            ->add(
-                'caption',
-                TextType::class,
-                ['label' => 'task.caption.label', 'help' => 'task.caption.help']
-            )
-            ->add(
-                'assignedTo',
-                UserSelectType::class,
-                ['label' => 'task.assignedTo.label', 'help' => 'task.assignedTo.help']
-            )
-            ->add(
-                'description',
-                TextareaType::class,
-                [
-                    'label' => 'task.description.label',
-                    'help' => 'task.description.help',
-                    'attr' => ['rows' => 25],
-                    'required' => false,
-                    'empty_data' => '',
-                ]
-            );
-    }
-
-    protected function buildDictionaryFields(FormBuilderInterface $builder, string $scenario): void
-    {
-        $builder
-            ->add(
-                'stage',
-                DictionaryStageSelectType::class,
-                [
-                    'label' => 'task.stage.label',
-                    'help' => 'task.stage.help',
-                    'scenario' => $scenario,
-                ]
-            )
-            ->add(
-                'type',
-                DictionarySelectType::class,
-                [
-                    'label' => 'task.type.label',
-                    'help' => 'task.type.help',
-                    'dictionary' => TypesEnum::TASK_TYPE()
-                ]
-            )
-            ->add(
-                'priority',
-                DictionarySelectType::class,
-                [
-                    'label' => 'task.priority.label',
-                    'help' => 'task.priority.help',
-                    'dictionary' => TypesEnum::TASK_PRIORITY()
-                ]
-            )
-            ->add(
-                'complexity',
-                DictionarySelectType::class,
-                [
-                    'label' => 'task.complexity.label',
-                    'help' => 'task.complexity.help',
-                    'dictionary' => TypesEnum::TASK_COMPLEXITY()
-                ]
-            );
-
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) {
-                $entity = $event->getData();
-                $dictionaries = $this->dictionaryFetcher->getDictionariesByEntityClass(Task::class, $entity);
-                if ($dictionaries[TypesEnum::TASK_STAGE]) {
-                    if (!$dictionaries[TypesEnum::TASK_STAGE]->isEnabled()) {
-                        $event->getForm()->remove('stage');
-                    } elseif ($entity instanceof HasClosedStatusInterface && $entity->isClosed()) {
-                        // так как опции уже добавленных контролов менять нельзя, пересоздаем контрол выбора этапа
-                        // с новыми данными
-                        $stageTypeConfig = $event->getForm()->get('stage')->getConfig()->getOptions();
-                        unset($stageTypeConfig['choices']);
-                        $stageTypeConfig['scenario'] = DictionaryStageSelectType::SCENARIO_CLOSE;
-
-                        $event->getForm()->remove('stage');
-                        $event->getForm()->add(
-                            'stage',
-                            DictionaryStageSelectType::class,
-                            $stageTypeConfig
-                        );
-                    }
-
-                    if (!$dictionaries[TypesEnum::TASK_TYPE]->isEnabled()) {
-                        $event->getForm()->remove('type');
-                    }
-                    if (!$dictionaries[TypesEnum::TASK_PRIORITY]->isEnabled()) {
-                        $event->getForm()->remove('priority');
-                    }
-                    if (!$dictionaries[TypesEnum::TASK_COMPLEXITY]->isEnabled()) {
-                        $event->getForm()->remove('complexity');
-                    }
-                }
-            }
-        );
-    }
 }
--- src/Form/Type/Task/NewTaskType.php
+++ src/Form/Type/Task/NewTaskType.php
@@ -8,7 +8,10 @@ declare(strict_types=1);
 
 namespace App\Form\Type\Task;
 
+use App\Dictionary\Fetcher;
 use App\Dictionary\TypesEnum;
+use App\Entity\Contract\HasClosedStatusInterface;
+use App\Entity\Task;
 use App\Form\DTO\Task\NewTaskDTO;
 use App\Form\Type\Base\DictionarySelectType;
 use App\Form\Type\Base\DictionaryStageSelectType;
@@ -17,10 +20,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class NewTaskType extends EditTaskType
+class NewTaskType extends AbstractType
 {
+    protected Fetcher $dictionaryFetcher;
+
+    public function __construct(Fetcher $dictionaryFetcher)
+    {
+        $this->dictionaryFetcher = $dictionaryFetcher;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $this->buildCommonFields($builder);
@@ -30,5 +42,109 @@ class NewTaskType extends EditTaskType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefault('data_class', NewTaskDTO::class);
+    }
+
+
+    protected function buildCommonFields(FormBuilderInterface $builder): void
+    {
+        $builder
+            ->add(
+                'caption',
+                TextType::class,
+                ['label' => 'task.caption.label', 'help' => 'task.caption.help']
+            )
+            ->add(
+                'assignedTo',
+                UserSelectType::class,
+                ['label' => 'task.assignedTo.label', 'help' => 'task.assignedTo.help']
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                [
+                    'label' => 'task.description.label',
+                    'help' => 'task.description.help',
+                    'attr' => ['rows' => 25],
+                    'required' => false,
+                    'empty_data' => '',
+                ]
+            );
+    }
+
+    protected function buildDictionaryFields(FormBuilderInterface $builder, string $scenario): void
+    {
+        $builder
+            ->add(
+                'stage',
+                DictionaryStageSelectType::class,
+                [
+                    'label' => 'task.stage.label',
+                    'help' => 'task.stage.help',
+                    'scenario' => $scenario,
+                ]
+            )
+            ->add(
+                'type',
+                DictionarySelectType::class,
+                [
+                    'label' => 'task.type.label',
+                    'help' => 'task.type.help',
+                    'dictionary' => TypesEnum::TASK_TYPE()
+                ]
+            )
+            ->add(
+                'priority',
+                DictionarySelectType::class,
+                [
+                    'label' => 'task.priority.label',
+                    'help' => 'task.priority.help',
+                    'dictionary' => TypesEnum::TASK_PRIORITY()
+                ]
+            )
+            ->add(
+                'complexity',
+                DictionarySelectType::class,
+                [
+                    'label' => 'task.complexity.label',
+                    'help' => 'task.complexity.help',
+                    'dictionary' => TypesEnum::TASK_COMPLEXITY()
+                ]
+            );
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) {
+                $entity = $event->getData();
+                $dictionaries = $this->dictionaryFetcher->getDictionariesByEntityClass(Task::class, $entity);
+                if ($dictionaries[TypesEnum::TASK_STAGE]) {
+                    if (!$dictionaries[TypesEnum::TASK_STAGE]->isEnabled()) {
+                        $event->getForm()->remove('stage');
+                    } elseif ($entity instanceof HasClosedStatusInterface && $entity->isClosed()) {
+                        // так как опции уже добавленных контролов менять нельзя, пересоздаем контрол выбора этапа
+                        // с новыми данными
+                        $stageTypeConfig = $event->getForm()->get('stage')->getConfig()->getOptions();
+                        unset($stageTypeConfig['choices']);
+                        $stageTypeConfig['scenario'] = DictionaryStageSelectType::SCENARIO_CLOSE;
+
+                        $event->getForm()->remove('stage');
+                        $event->getForm()->add(
+                            'stage',
+                            DictionaryStageSelectType::class,
+                            $stageTypeConfig
+                        );
+                    }
+
+                    if (!$dictionaries[TypesEnum::TASK_TYPE]->isEnabled()) {
+                        $event->getForm()->remove('type');
+                    }
+                    if (!$dictionaries[TypesEnum::TASK_PRIORITY]->isEnabled()) {
+                        $event->getForm()->remove('priority');
+                    }
+                    if (!$dictionaries[TypesEnum::TASK_COMPLEXITY]->isEnabled()) {
+                        $event->getForm()->remove('complexity');
+                    }
+                }
+            }
+        );
     }
 }
