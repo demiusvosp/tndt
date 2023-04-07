1b597004fdb7473bac6153b98bb9975d903e9c4a
Dmitriy Vospennikov <demius.vosp@gmail.com>
2023-04-07 22:36:01 +0300

    tndt-96 Добавить в проект Спецификации и перевести на них репозитории. CommentRepository - done.

---
+++ src/Specification/Comment/ByOwnerSpec.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User: demius
+ * Date: 07.04.2023
+ * Time: 22:21
+ */
+
+namespace App\Specification\Comment;
+
+use App\Entity\Contract\CommentableInterface;
+use Happyr\DoctrineSpecification\Spec;
+use Happyr\DoctrineSpecification\Specification\BaseSpecification;
+
+class ByOwnerSpec extends BaseSpecification
+{
+    private CommentableInterface $owner;
+
+    public function __construct(CommentableInterface $owner, ?string $context = null)
+    {
+        $this->owner = $owner;
+        parent::__construct($context);
+    }
+
+    protected function getSpec()
+    {
+        return Spec::andX(
+            Spec::eq('entity_type', get_class($this->owner)),
+            Spec::eq('entity_id', $this->owner->getId())
+        );
+    }
+}
