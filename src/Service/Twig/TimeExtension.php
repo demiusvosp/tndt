5704958103d6fa77e4997051a2812587ba61288e
Dmitriy Vospennikov <demius.vosp@gmail.com>
2023-04-24 01:24:17 +0300

    tndt-25 Гибкое описание времени. Добавляем Intl-extension, выводим в попапе абсолютное время

--- src/Service/Twig/TimeExtension.php
+++ src/Service/Twig/TimeExtension.php
@@ -11,16 +11,24 @@ namespace App\Service\Twig;
 use Knp\Bundle\TimeBundle\DateTimeFormatter;
 use Knp\Bundle\TimeBundle\Twig\Extension\TimeExtension as BaseTimeExtension;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Environment;
+use Twig\Error\RuntimeError;
+use Twig\Extra\Intl\IntlExtension;
 use Twig\TwigFilter;
 
 class TimeExtension extends BaseTimeExtension
 {
     protected $translator;
+    protected IntlExtension $intlExtension;
 
-    public function __construct(DateTimeFormatter $formatter, TranslatorInterface $translator)
+    public function __construct(
+        DateTimeFormatter $formatter,
+        TranslatorInterface $translator,
+        IntlExtension $intlExtension)
     {
         parent::__construct($formatter);
         $this->translator = $translator;
+        $this->intlExtension = $intlExtension;
     }
 
     public function getFilters(): array
@@ -28,18 +36,39 @@ class TimeExtension extends BaseTimeExtension
         return [
             new TwigFilter(
                 'ago',
+                [$this, 'ago'],
+                ['is_safe' => ['html'], 'needs_environment' => true]
+            ),
+            new TwigFilter(
+                'time_diff',
                 [$this, 'diff'],
                 ['is_safe' => ['html']]
             ),
         ];
     }
 
-    public function diff($since = null, $to = null, $locale = null): string
+    /**
+     * @throws RuntimeError
+     */
+    public function ago(Environment $env, $date = null, $html = true): string
+    {
+        $string = $this->diff($date, null, $html);
+        if($html) {
+            $string = sprintf(
+                '<span title="%s">%s</span>',
+                $this->intlExtension->formatDateTime($env, $date),
+                $string
+            );
+        }
+        return $string;
+    }
+
+    public function diff($since = null, $to = null, $html = true): string
     {
         if ($since == null) {
             return '<i>' . $this->translator->trans('never') . '</i>';
         }
-        return parent::diff($since, $to, $locale);
+        return parent::diff($since, $to);
     }
 
     /**
