5a3b02f501f3802f9ae8e9671ca8fad3f96dde66
Dmitriy Vospennikov <demius.vosp@gmail.com>
2024-01-29 00:49:42 +0300

    tndt-141 Уйти от меню adminlte на собственное. Переводим хлебные крошки, крошки страниц в проекте.

--- src/EventSubscriber/Menu/Breadcrumbs/ProjectItemsSubscriber.php
+++ src/EventSubscriber/Menu/Breadcrumbs/ProjectItemsSubscriber.php
@@ -14,15 +14,25 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use function str_starts_with;
 
 class ProjectItemsSubscriber implements EventSubscriberInterface
 {
     private ProjectContext $projectContext;
+    private RequestStack $requestStack;
+    private TranslatorInterface $translator;
     private UrlGeneratorInterface $router;
 
-    public function __construct(ProjectContext $projectContext)
-    {
+    public function __construct(
+        ProjectContext $projectContext,
+        RequestStack $requestStack,
+        TranslatorInterface $translator,
+        UrlGeneratorInterface $router
+    ) {
         $this->projectContext = $projectContext;
+        $this->requestStack = $requestStack;
+        $this->translator = $translator;
+        $this->router = $router;
     }
 
     public static function getSubscribedEvents(): array
@@ -34,12 +44,36 @@ class ProjectItemsSubscriber implements EventSubscriberInterface
 
     public function buildBreadcrumb(MenuEvent $event): void
     {
+        $route = $this->requestStack->getMainRequest()?->get('_route');
         $project = $this->projectContext->getProject();
-        if ($project) {
+        if (!$project) {
+            return;
+        }
+
+        $event->addItem(new BaseMenuItem(
+            'project',
+            $project->getName(),
+            $this->router->generate('project.index', ['suffix' => $project->getSuffix()])
+        ));
+        if (str_starts_with($route, 'project.edit')) {
             $event->addItem(new BaseMenuItem(
-                'project',
-                $project->getName(),
-                $this->router->generate('project.index', ['suffix' => $project->getSuffix()])
+                'project.edit',
+                $this->translator->trans('breadcrumb.project.edit.common'),
+                $this->router->generate('project.edit', ['suffix' => $project->getSuffix()])
+            ));
+        }
+        if (str_starts_with($route, 'task.')) {
+            $event->addItem(new BaseMenuItem(
+                'tasks',
+                $this->translator->trans('breadcrumb.project.tasks'),
+                $this->router->generate('task.list', ['suffix' => $project->getSuffix()])
+            ));
+        }
+        if (str_starts_with($route, 'doc.')) {
+            $event->addItem(new BaseMenuItem(
+                'doc',
+                $this->translator->trans('breadcrumb.project.docs'),
+                $this->router->generate('doc.list', ['suffix' => $project->getSuffix()])
             ));
         }
     }
