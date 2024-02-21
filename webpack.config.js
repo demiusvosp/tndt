955cd57717c4cf8940affe0571a0a2d7753338a5
Dmitriy Vospennikov <demius.vosp@gmail.com>
2024-02-22 00:04:12 +0300

    tndt-147 tndt-147 - [Переход с AdminLTE на Tabler] - Страницы внутри проекта. Продолжая оттачиваться хлебные крошки

--- webpack.config.js
+++ webpack.config.js
@@ -81,15 +81,10 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
     //.enableIntegrityHashes(Encore.isProduction())
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
