4b885974a055306c175093a999342e7b79e21776
Dmitriy Vospennikov <demius.vosp@gmail.com>
2024-05-09 19:34:20 +0300

    tndt-160 Удалил пару уже явно не нужных пакетов

--- config/dev/bundles.php
+++ config/dev/bundles.php
@@ -4,5 +4,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
