7ea2ddce9a33d3c27f6c5ca0e27b45453cadb715
Dmitriy Vospennikov <demius.vosp@gmail.com>
2024-06-12 13:57:50 +0300

    tndt-9 wiki-разметка. Этап 0. Собственный рендерер который настраивает commonMark

---
+++ src/Service/Wiki/MarkdownConverter.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * User: demius
+ * Date: 12.06.2024
+ * Time: 13:34
+ */
+
+namespace App\Service\Wiki;
+
+use App\Exception\WikiConvertException;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Exception\CommonMarkException;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use Twig\Extra\Markdown\MarkdownInterface;
+use League\CommonMark\MarkdownConverter as LeagueCommonMarkConverter;
+use function dump;
+use function get_class;
+
+class MarkdownConverter implements MarkdownInterface
+{
+    private LeagueCommonMarkConverter $converter;
+
+    public function __construct()
+    {
+        $environment = new Environment();
+        $this->initializeEnvironment($environment);
+
+        $this->converter = new LeagueCommonMarkConverter($environment);
+    }
+
+    public function convert(string $body): string
+    {
+        try {
+            return $this->converter->convert($body);
+        } catch (CommonMarkException $e) {
+            throw new WikiConvertException($e);
+        }
+    }
+
+    private function initializeEnvironment(Environment $environment): void
+    {
+        $environment->addExtension(new CommonMarkCoreExtension());
+    }
+}
