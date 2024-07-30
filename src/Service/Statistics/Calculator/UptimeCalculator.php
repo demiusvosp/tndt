5ec02d2c50b36f0642ec7e687f099a33477015a2
Dmitriy Vospennikov <demius.vosp@gmail.com>
2024-07-30 10:34:42 +0300

    tndt-153 Статистика. Расчитываем uptime контейнера, а не хоста

--- src/Service/Statistics/Calculator/UptimeCalculator.php
+++ src/Service/Statistics/Calculator/UptimeCalculator.php
@@ -10,13 +10,16 @@ namespace App\Service\Statistics\Calculator;
 use App\Service\Statistics\CalculatorInterface;
 use DateTimeImmutable;
 use function file_get_contents;
+use function preg_match;
+use function shell_exec;
 
 class UptimeCalculator implements CalculatorInterface
 {
     public function calculate()
     {
-        [$seconds, ] = explode(' ', file_get_contents('/proc/uptime'));
+        $output = shell_exec('stat /proc/1');
+        preg_match('/Modify: ([\d\-: .]+)/', $output, $uptimeDate);
 
-        return DateTimeImmutable::createFromFormat('U', time() - (int)$seconds);
+        return new DateTimeImmutable($uptimeDate[1]);
     }
 }
