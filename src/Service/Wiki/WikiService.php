d22d871d21fb23db5e218b5339edfed8782a7610
Dmitriy Vospennikov <demius.vosp@gmail.com>
2025-01-27 00:00:01 +0300

    tndt-173 [wiki] ссылка на проект.  Простая реализация в лоб

--- src/Service/Wiki/WikiService.php
+++ src/Service/Wiki/WikiService.php
@@ -8,12 +8,19 @@
 namespace App\Service\Wiki;
 
 use App\Entity\Doc;
+use App\Entity\Project;
 use App\Entity\Task;
 use App\Model\Dto\WikiLink;
 use App\Model\Enum\Wiki\LinkStyleEnum;
-use App\Repository\DocRepository;
-use App\Repository\TaskRepository;
+use App\Specification\Doc\ByDocIdSpec;
+use App\Specification\Project\ByIdSpec;
+use App\Specification\Task\ByTaskIdSpec;
+use Doctrine\ORM\EntityManagerInterface;
 use Happyr\DoctrineSpecification\Exception\NoResultException;
+use Happyr\DoctrineSpecification\Repository\EntitySpecificationRepositoryInterface;
+use Happyr\DoctrineSpecification\Spec;
+use Happyr\DoctrineSpecification\Specification\Specification;
+use InvalidArgumentException;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
@@ -27,8 +34,7 @@ class WikiService
 {
     private UrlGeneratorInterface $router;
     private TranslatorInterface $translator;
-    private TaskRepository $taskRepository;
-    private DocRepository $docRepository;
+    private EntityManagerInterface $entityManager;
     private LoggerInterface $logger;
 
     private array $entityCache;
@@ -36,39 +42,57 @@ class WikiService
     public function __construct(
         UrlGeneratorInterface $router,
         TranslatorInterface $translator,
-        TaskRepository $taskRepository,
-        DocRepository $docRepository,
+        EntityManagerInterface $entityManager,
         LoggerInterface $logger
     ) {
         $this->router = $router;
         $this->translator = $translator;
-        $this->taskRepository = $taskRepository;
-        $this->docRepository = $docRepository;
+        $this->entityManager = $entityManager;
         $this->logger = $logger;
     }
 
     public function getWikiLinkRegEx()
     {
-        return implode('|', [Task::TASKID_REGEX, Doc::DOCID_REGEX]);
+        return implode('|', [Task::TASKID_REGEX, Doc::DOCID_REGEX, 'p:\w+']);
     }
 
     public function getLink(string $linkTag): ?WikiLink
     {
         if (preg_match('/(\w+)-(\d+)/', $linkTag, $matches)) {
-            return $this->createTaskLink($linkTag, $matches[1], $matches[2]);
+            return $this->createTaskLink($matches[1], $matches[2]);
         }
         if (preg_match('/(\w+)#(\w+)/', $linkTag, $matches)) {
-            return $this->createDocLink($linkTag, $matches[1], $matches[2]);
+            return $this->createDocLink($matches[1], $matches[2]);
+        }
+        if (preg_match('/(\w+):(\w+)/', $linkTag, $matches) && $matches[1] == 'p') {
+            return $this->createProjectLink($matches[2]);
         }
         return null;
     }
 
-    private function createTaskLink(string $linkTag, string $suffix, string $taskNo)
+    private function getEntityByLink(string $linkTag, string $class, Specification $spec): ?object
     {
-        if (!isset($this->entityCache[$linkTag])) {
-            $this->entityCache[$linkTag] = $this->taskRepository->findByTaskId($linkTag);
+        $repository = $this->entityManager->getRepository($class);
+        if (!$repository instanceof EntitySpecificationRepositoryInterface) {
+            throw new InvalidArgumentException('Cannot get EntitySpecificationRepository for "'.$linkTag.'"');
         }
-        $task = $this->entityCache[$linkTag];
+        if (!isset($this->entityCache[$linkTag])) {
+            try {
+                $this->entityCache[$linkTag] = $repository->matchSingleResult($spec);
+            } catch (NoResultException $e) {
+                $this->entityCache[$linkTag] = null;
+            }
+        }
+        return $this->entityCache[$linkTag];
+    }
+
+    private function createTaskLink(string $suffix, string $taskNo)
+    {
+        // то вместе с Task::explodeTaskId() и регулярками сверху должно уехать в енам LinkTypes
+        $linkTag = $suffix . '-' . $taskNo;
+
+        /** @var Task $task */
+        $task = $this->getEntityByLink($linkTag, Task::class, new ByTaskIdSpec($linkTag));
         if (!$task) {
             $this->logger->warning('Task not found', ['link' => $linkTag]);
             return new WikiLink(
@@ -86,12 +110,12 @@ class WikiService
         );
     }
 
-    private function createDocLink(string $linkTag, string $suffix, string $docNo): ?WikiLink
+    private function createDocLink(string $suffix, string $docNo): ?WikiLink
     {
-        if (!isset($this->entityCache[$linkTag])) {
-            $this->entityCache[$linkTag] = $this->docRepository->findByDocId($linkTag);
-        }
-        $doc = $this->entityCache[$linkTag];
+        // это вместе с Doc::explodeDocId() и регулярками сверху должно уехать в енам LinkTypes
+        $linkTag = $suffix . '#' . $docNo;
+        /** @var Doc $doc */
+        $doc = $this->getEntityByLink($linkTag, Doc::class, new ByDocIdSpec($linkTag));
         if (!$doc) {
             $this->logger->warning('Document not found', ['link' => $linkTag]);
             return new WikiLink(
@@ -106,6 +130,28 @@ class WikiService
             $doc->getDocId() . ' - ' . $doc->getCaption(),
             $doc->isArchived() ? LinkStyleEnum::DocArchived : LinkStyleEnum::Normal,
             $doc->getCaption()
+        );
+    }
+
+    private function createProjectLink(string $suffix)
+    {
+        $linkTag = 'p:' . $suffix;
+        /** @var Project $project */
+        $project = $this->getEntityByLink($linkTag, Project::class, new ByIdSpec($suffix));
+        if (!$project) {
+            $this->logger->warning('Project not found', ['link' => $linkTag]);
+            return new WikiLink(
+                $this->router->generate('project.list'),
+                $linkTag,
+                LinkStyleEnum::NotFound,
+                $this->translator->trans('project.not_found')
+            );
+        }
+        return new WikiLink(
+            $this->router->generate('project.index', ['suffix' => $suffix]),
+            $project->getSuffix(),
+            $project->isArchived() ? LinkStyleEnum::DocArchived : LinkStyleEnum::Normal,
+            $project->getName()
         );
     }
 }
